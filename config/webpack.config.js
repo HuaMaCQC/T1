dfbe0a4d10757fa9a662b20cd9bb3165b3de0965
HuaMa <knid855jieh8d56201@gmail.com>
2019-06-13 04:19:58 +0800

    設定styled-jsx套件

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -353,6 +353,7 @@ module.exports = function(webpackEnv) {
                       },
                     },
                   ],
+                  "styled-jsx/babel"
                 ],
                 // This is a feature of `babel-loader` for webpack (not Babel itself).
                 // It enables caching results in ./node_modules/.cache/babel-loader/
@@ -395,18 +396,37 @@ module.exports = function(webpackEnv) {
             // to a file, but in development "style" loader enables hot editing
             // of CSS.
             // By default we support CSS Modules with the extension .module.css
+            // {
+            //   test: cssRegex,
+            //   exclude: cssModuleRegex,
+            //   use: getStyleLoaders({
+            //     importLoaders: 1,
+            //     sourceMap: isEnvProduction && shouldUseSourceMap,
+            //   }),
+            //   // Don't consider CSS imports dead code even if the
+            //   // containing package claims to have no side effects.
+            //   // Remove this when webpack adds a warning or an error for this.
+            //   // See https://github.com/webpack/webpack/issues/6571
+            //   sideEffects: true,
+            // },
             {
               test: cssRegex,
-              exclude: cssModuleRegex,
-              use: getStyleLoaders({
-                importLoaders: 1,
-                sourceMap: isEnvProduction && shouldUseSourceMap,
-              }),
-              // Don't consider CSS imports dead code even if the
-              // containing package claims to have no side effects.
-              // Remove this when webpack adds a warning or an error for this.
-              // See https://github.com/webpack/webpack/issues/6571
-              sideEffects: true,
+              use: [{
+                loader: 'babel-loader',
+                options: {
+                  babelrc: false,
+                  plugins: [
+                    require.resolve('styled-jsx/babel'),
+                  ]
+                }
+              },
+              {
+                loader: require('styled-jsx/webpack').loader,
+                options: {
+                  type: 'scoped'
+                }
+              }
+              ]
             },
             // Adds support for CSS Modules (https://github.com/css-modules/css-modules)
             // using the extension .module.css
